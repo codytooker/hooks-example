f793e89ef9348f7917e1291d165a16323a1ba871
Cody <cody@dreamersofday.co>
2019-04-07 12:41:58 -0500

    Updates

---
+++ precache-manifest.b32f6a9b932dc8a9c8a407c6e1a21200.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "535eb5b76dff9b53fc52",
+    "url": "/codytooker/hooks-example/static/js/runtime~main.8e405ae7.js"
+  },
+  {
+    "revision": "a2313e29e54d9de4da24",
+    "url": "/codytooker/hooks-example/static/js/main.b622e7a4.chunk.js"
+  },
+  {
+    "revision": "60a2a338fdfbddbdc4be",
+    "url": "/codytooker/hooks-example/static/js/2.b41fd95e.chunk.js"
+  },
+  {
+    "revision": "a64698bd932e3a0d588af076f710fe79",
+    "url": "/codytooker/hooks-example/index.html"
+  }
+];
